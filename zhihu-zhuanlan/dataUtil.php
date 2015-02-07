d10fbccb23f5a8600f8198f2dd8dcd31981f36ec
Wzy <wzywangzhiyong@gmail.com>
2015-02-07 22:29:10 +0800

    [rss]Add gzip support

--- zhihu-zhuanlan/dataUtil.php
+++ zhihu-zhuanlan/dataUtil.php
@@ -6,11 +6,16 @@ function get_json($url)
 {
   for ($i = 0; $i < RECONNECT_TIMES; $i++) {
     $raw_data = file_get_contents($url);
+    $raw_content = gzdecode($raw_data);
 
-    if (!$raw_data) {
+    if (!$raw_content) {
+      $raw_content = $raw_data;
+    }
+
+    if (!$raw_content) {
       continue;
     } else {
-      $json = json_decode($raw_data, true);
+      $json = json_decode($raw_content, true);
       if ($json == NULL) {
         continue;
       } else {
