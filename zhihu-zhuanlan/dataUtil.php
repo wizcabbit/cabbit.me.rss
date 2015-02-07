8d30cffbce47e7c88338322cee26a67725eefb01
Wzy <wzywangzhiyong@gmail.com>
2015-02-07 22:57:20 +0800

    [rss]Add 6 hour cache

--- zhihu-zhuanlan/dataUtil.php
+++ zhihu-zhuanlan/dataUtil.php
@@ -5,9 +5,11 @@ define("RECONNECT_TIMES", 5);
 function get_json($url)
 {
   for ($i = 0; $i < RECONNECT_TIMES; $i++) {
+    // 获取接口数据，按gzip解压
     $raw_data = file_get_contents($url);
     $raw_content = gzdecode($raw_data);
 
+    // gzip解压失败后，直接使用结果
     if (!$raw_content) {
       $raw_content = $raw_data;
     }
--- zhihu-zhuanlan/smartyUtil.php
+++ zhihu-zhuanlan/smartyUtil.php
@@ -5,7 +5,10 @@ require_once('smarty/Smarty.class.php');
 
 $smarty = new Smarty;
 $smarty->debugging = false;
-$smarty->caching = false;
+$smarty->caching = true;
+
+// Atom缓存时间：6小时
+$smarty->cache_lifetime = "21600";
 
 // Initialize smarty directory configurations
 $smarty->template_dir = './';
