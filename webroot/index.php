1b10d7488e15dd64355e13001a109a8a486ed999
root <root@c0a39d23-8b07-4e72-9465-0b54253d0f4c>
2015-04-02 16:33:55 +0000

    create default index
    git-svn-id: http://repo1.svn.ace.aliyun.com/svn/1285103398001438/55048/1@45 c0a39d23-8b07-4e72-9465-0b54253d0f4c

---
+++ webroot/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>欢迎使用 ACE</title>
+</head>
+<body>
+<h2>
+欢迎使用 ACE
+</h2>
+<?php
+echo "当前时间：" . date("Y-m-d H:i:s");
+echo "<br/>";
+echo "<br/>小提示:<br/>";
+echo "- webroot 是您的web目录，该目录下的所有文件都可以被浏览器直接访问";
+echo "<br/>";
+echo "- 本页面位置 /webroot/index.php，您可以进行修改";
+echo "<br/>";
+echo "- 敏感代码可放置在 svn 根目录下,与 webroot 平级";
+?>
+</body>
+</html>
